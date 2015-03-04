81da204397393820f2e77c002a48cf1501796666
Emil Rakadjiev <emil.rakadjiev.bf@hitachi.com>
2015-03-04 10:39:47 +0900

    zmq_asio_socket is now a class that derives from an Asio socket (basic_stream_descriptor) and stores a reference to the ZMQ socket. As before, the file descriptor of the ZMQ socket is assigned to the Asio socket.

--- zmq_asio_socket.hpp
+++ zmq_asio_socket.hpp
@@ -8,6 +8,7 @@
 #ifndef ZMQ_ASIO_SOCKET_HPP_
 #define ZMQ_ASIO_SOCKET_HPP_
 
+#include <czmq.h>
 #include <boost/asio/io_service.hpp>
 #include <boost/asio/posix/stream_descriptor.hpp>
 #include <boost/asio/posix/stream_descriptor_service.hpp>
@@ -22,6 +23,14 @@ public:
 	}
 };
 
-typedef boost::asio::posix::basic_stream_descriptor<non_closing_service> zmq_asio_socket;
+class zmq_asio_socket : public boost::asio::posix::basic_stream_descriptor<non_closing_service>{
+public:
+	zmq_asio_socket(zsock_t* zmq_socket, boost::asio::io_service& io_service): boost::asio::posix::basic_stream_descriptor<non_closing_service>(io_service, zsock_fd(zmq_socket)), zmq_socket(zmq_socket){}
+	zsock_t* get_zmq_socket(){
+	  return zmq_socket;
+	}
+private:
+	zsock_t* zmq_socket;
+};
 
 #endif /* ZMQ_ASIO_SOCKET_HPP_ */
